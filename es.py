d0676cb07208f04aabe572b27c49736315edcecd
Blunce <756892169@qq.com>
2015-04-19 11:23:26 +0800

    final coding

--- es.py
+++ es.py
@@ -10,8 +10,8 @@ import math
 import matplotlib.pyplot as plt
 
 PRECISION = 6
-FATHER, SON = 30, 40  # (FATHER + SON)
-ITERATE_NUM = 500
+FATHER, SON = 100, 100  # (FATHER + SON)
+ITERATE_NUM = 100
 RATE = 0.2  # 突变在子代中发生的概率
 
 def f(x1, x2):
@@ -21,14 +21,20 @@ def numToStr(x, y):
     # 返回字符串
     x, y = x * 10 ** PRECISION, y * 10 ** PRECISION
     if x >= 0:
-        xstr = '0' + bin(x)
+        xstr = '0' + bin(int(x))
     else:
-        xstr = '1' + bin(abs(x))
+        xstr = '1' + bin(abs(int(x)))
         
     if y >= 0:
-        ystr = '0' + bin(y)
+        ystr = '0' + bin(int(y))
     else:
-        ystr = '1' + bin(abs(y))
+        ystr = '1' + bin(abs(int(y)))
+        
+    if 26 - len(xstr) != 0:
+        xstr = xstr[:3] + '0' * (26 - len(xstr)) + xstr[3:]
+    if 26 - len(ystr) != 0:
+        ystr = ystr[:3] + '0' * (26 - len(ystr)) + ystr[3:]
+        
     return xstr + ',' + ystr
 
 
@@ -37,12 +43,12 @@ def strToNum(aim):
     if aim[0][0] == '0':
         aim[0] = aim[0][1:]
     else:
-        aim[0][0] = '-'
+        aim[0] = '-' + aim[0][1:]
     if aim[1][0] == '0':
         aim[1] = aim[1][1:]
     else:
-        aim[1][0] = '-'
-    return [int(aim[0]) / float(10 ** PRECISION), int(aim[1]) / float(10 ** PRECISION)]
+        aim[1] = '-' + aim[1][1:]
+    return [int(aim[0], 2) / float(10 ** PRECISION), int(aim[1], 2) / float(10 ** PRECISION)]
    
    
 def encode(dataSet):
@@ -59,9 +65,9 @@ def decode(dataSet):
     return aimList
 
 
-def InitialGen(num=FATHER):
+def InitialGen():
     sons = []
-    for i in range(num):
+    for i in range(FATHER):
         sons.append([random.uniform(-5, 5), random.uniform(-5, 5)])
     return sons
 
@@ -101,12 +107,16 @@ def mutation(dataSet):
     random.shuffle(dataSet)
     for i in range(int(SON * RATE)):
         temp = range(0, 53)
-        del temp[1, 2, 26, 28, 29]
+        del temp[29]  # 1, 2, 26, 28, 29
+        del temp[28]
+        del temp[26]
+        del temp[2]
+        del temp[1]
         random.shuffle(temp)
         aim = dataSet[i]
         aim = list(aim)
-        aim[temp[0]] = str(int((aim[temp[0]]) + 1) % 2)
-        dataSet[i]=''.join(aim)
+        aim[temp[0]] = str((int(aim[temp[0]]) + 1) % 2)
+        dataSet[i] = ''.join(aim)
     return dataSet
 
 
@@ -116,9 +126,20 @@ def produceNextGen(dataSet):
     dataSet.extend(sonDataSet)
     return dataSet
 
+def deleteAbnormal(dataSet):
+    aim = []
+    for i in range(len(dataSet)):
+        if dataSet[i][0] > 5 or dataSet[i][0] < -5 or dataSet[i][1] > 5 or dataSet[i][1] < -5:
+            aim.append(i)
+    aim = aim[::-1]
+    print aim
+    for item in aim:
+        del dataSet[item]
+    return dataSet
 
 def NaturalSelection(dataSet):
     dataSet = decode(dataSet)
+    dataSet = deleteAbnormal(dataSet)
     dataSet = sorted(dataSet, key=lambda x:x[2], reverse=False)
     dataSet = dataSet[:FATHER][:]
     return encode(dataSet)
@@ -130,20 +151,23 @@ def Label(dataSet):
     dataSet = decode(dataSet)
     dataSet = np.array(dataSet)
     dataSet = dataSet[:][2]
-    return np.mean(dataSet), np.std(dataSet)
+    return  np.mean(dataSet)   ,np.std(dataSet)
 
     
 if __name__ == '__main__':
     dataSet = encode(InitialGen())
     y = []
     for i in range(ITERATE_NUM):
+        print i
         dataSet = produceNextGen(dataSet)
         dataSet = NaturalSelection(dataSet)
         y.append(Label(dataSet))
+        
     
-    x = range(1, 501)
+    x = range(1, ITERATE_NUM + 1)
     fig = plt.figure()
     ax = fig.add_subplot(111)
     ax.plot(x, y)
-    fig.show()
-    
+    print 'save result'
+    fig.savefig('result.jpg')
+    print 'end'
